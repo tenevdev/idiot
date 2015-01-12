44f1de9dcc5ea033471e33b3454e8d054dab27df
Tencho Tenev <tenev.dev@outlook.com>
2015-01-12 23:57:29 +0200

    Add body parser and connect to db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 var app = require('express')(),
+    env = app.get('env'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     logger = require('morgan'),
     getConfigurationValue = require('./config').getValueForEnvironment
 
 // TODO: Connect to database here
+mongoose.connect(getConfigurationValue('/database', env))
 
 // TODO: Setup express application here
-app.set('port', getConfigurationValue('/app/port', app.get('env')))
+app.set('port', getConfigurationValue('/app/port', env))
 
-if (app.get('env') === 'development') {
+app.use(bodyParser.json())
+
+if (env === 'development') {
     app.use(logger('dev'))
 }
 app.use(require('./routes'))
