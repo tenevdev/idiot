7874669e607bf17d5da5ff823a21e3dfd994b533
Alexander <alexander.zahariev1996@gmail.com>
2015-02-19 15:23:24 +0200

    Front-end structure edit

--- server/routes/views/index.js
+++ server/routes/views/index.js
@@ -2,4 +2,8 @@ var ViewsRouter = require('express').Router({
     mergeParams: true
 })
 
+ViewsRouter.get('/partials/:partialName', function(req, res, next){
+	res.render('../../public/views/' + req.params.partialName + '.html')
+})
+
 module.exports = ViewsRouter
