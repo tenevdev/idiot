493c6ac3d7b80489b480e78426ec57f8b7a3052b
Tencho Tenev <tenev.dev@outlook.com>
2015-02-26 20:37:20 +0200

    Update register controller

--- public/scripts/controllers/user/register.js
+++ public/scripts/controllers/user/register.js
@@ -1,7 +1,24 @@
 define(['app'], function(app) {
-    var injectParams = ['$scope'],
-        RegisterController = function($scope) {
-            this.message = 'Hello, Register!'
+    var injectParams = ['$http', '$location'],
+        RegisterController = function($http, $location) {
+            this.submit = function() {
+                var self = this
+
+                $http.post('/api/users', {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email
+                }).success(function(userProfile, status) {
+                    // Redirect new user to home page
+                    AuthenticationService.setCredentials(self.username, self.password)
+                    $location.path('/')
+                }).error(function(response, status) {
+                    // Display error
+                    var errors = response.errors,
+                    message = response.message
+                    self.error = message
+                })
+            }
         }
 
     RegisterController.$inject = injectParams
