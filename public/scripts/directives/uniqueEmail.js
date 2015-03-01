cff043e19544b65df14d2dd6d419b3b495d88019
Tencho Tenev <tenev.dev@outlook.com>
2015-03-01 12:23:25 +0200

    Add unique email directive

---
+++ public/scripts/directives/uniqueEmail.js
@@ -0,0 +1,44 @@
+define(['app'], function(app) {
+    var injectParams = ['$http'],
+        uniqueEmailDirective = function($http) {
+
+            var link = function(scope, element, attrs, ctrl) {
+                scope.busy = false
+                scope.$watch(attrs.ngModel, function(value) {
+
+                    // Reset errors
+                    ctrl.$setValidity('unique', true)
+
+                    if (!value) {
+                        // No need to check
+                        return
+                    }
+
+                    scope.busy = true
+
+                    $http.get('/api/users?email=' + value)
+                        .success(function(data) {
+                            if (data.length > 0) {
+                                // A user with this email exists
+                                ctrl.$setValidity('unique', false)
+                                ctrl.$error.unique = true
+                            }
+                            scope.busy = false
+                        })
+                        .error(function(data) {
+                            // Server error
+                            scope.busy = false
+                        })
+                })
+            }
+
+            return {
+                require: 'ngModel',
+                link: link,
+            }
+        }
+
+    uniqueUsernameDirective.$inject = injectParams
+
+    app.register.directive('uniqueEmail', uniqueEmailDirective)
+})
